7c12dfc209cad9dfd9e7749b3373323c87708b48
Taylor Howell <howelltaylor195@gmail.com>
2022-09-01 20:42:28 -0500

    fixed issue with not deserializing properly

--- Alarm501Forms/Form1.cs
+++ Alarm501Forms/Form1.cs
@@ -11,7 +11,7 @@ namespace Alarm501Forms
 
         public Form1()
         {
-
+            AlarmsManager.Initialize();
             InitializeComponent();
 
             editSelectedToolStripMenuItem.Enabled = false;
